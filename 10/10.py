0389d409aa2d778ec2c8feaf632b9dd2186ddc55
Andrea Zucchelli <zukka77@gmail.com>
2022-12-10 12:45:27 +0100

    feat: day 10 tmp

---
+++ 10/10.py
@@ -0,0 +1,82 @@
+from pathlib import Path
+from typing import List
+from functools import reduce
+import logging
+from math import copysign
+import sys
+
+logging.basicConfig()
+log = logging.getLogger(__name__)
+log.setLevel(level=logging.DEBUG)
+
+
+def first_question(input_data: str) -> int:
+    X = 1
+    strengths = []
+    instruction_queue = []
+    lines = input_data.splitlines()
+    i = 0
+    cycle = 1
+    for (i, line) in enumerate(input_data.splitlines()):
+        line = lines[i]
+        if len(instruction_queue):
+            X += instruction_queue.pop(0)
+        cycle = i + 1
+        if cycle == 20 or (cycle - 20) % 40 == 0 and cycle <= 220:
+            strengths.append(cycle * X)
+        if line == "noop":
+            instruction_queue.append(0)
+        else:
+            quantity = int(line.split()[1])
+            instruction_queue = instruction_queue + [0, quantity]
+
+    while len(instruction_queue):
+        X += instruction_queue.pop(0)
+        cycle += 1
+        if cycle == 20 or (cycle - 20) % 40 == 0 and cycle <= 220:
+            strengths.append(cycle * X)
+    return sum(strengths)
+
+
+def second_question(input_data: str):
+    X = 1
+    instruction_queue = []
+    lines = input_data.splitlines()
+    i = 0
+    cycle = 0
+    r = c = 0
+    for (i, line) in enumerate(input_data.splitlines()):
+        line = lines[i]
+        if cycle % 40 == 0:
+            sys.stdout.write("\n")
+        if cycle // 40 == 6:
+            break
+        if len(instruction_queue):
+            X += instruction_queue.pop(0)
+        cycle = i + 1
+        if abs(((cycle - 1) % 40) - X) <= 1:
+            sys.stdout.write("#")
+        else:
+            sys.stdout.write(" ")
+        if line == "noop":
+            instruction_queue.append(0)
+        else:
+            quantity = int(line.split()[1])
+            instruction_queue = instruction_queue + [0, quantity]
+
+    while len(instruction_queue) and cycle // 40 < 6:
+        if cycle % 40 == 0:
+            sys.stdout.write("\n")
+        X += instruction_queue.pop(0)
+        cycle += 1
+        if abs(((cycle - 1) % 40) - X) <= 1:
+            sys.stdout.write("#")
+        else:
+            sys.stdout.write(" ")
+
+
+if __name__ == "__main__":
+
+    input_data = (Path(__file__).parent / "INPUT").read_text("UTF8")
+    print(first_question(input_data))
+    second_question(input_data)
---
+++ 10/10.rs
@@ -0,0 +1,243 @@
+use std::collections::HashSet;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn main() {
+    let mut lines = read_lines("./INPUT").expect("error reading file");
+    if let Ok(answer1) = first_question(lines) {
+        println!("{}", answer1);
+    };
+    lines = read_lines("./INPUT").expect("error reading file");
+    if let Ok(answer2) = second_question(lines) {
+        println!("{}", answer2);
+    };
+}
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where
+    P: AsRef<Path>,
+{
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+#[allow(dead_code)]
+fn move_tail_old(
+    head_position: &mut Vec<i32>,
+    tail_position: &mut Vec<i32>,
+    tail_positions: &mut HashSet<(i32, i32)>,
+) {
+    if head_position[1] - tail_position[1] > 1 {
+        //UP
+        if head_position[0] == tail_position[0] {
+            tail_position[1] += 1;
+        } else if head_position[0] > tail_position[0] {
+            tail_position[1] += 1;
+            tail_position[0] += 1;
+        } else {
+            tail_position[1] += 1;
+            tail_position[0] -= 1;
+        }
+    }
+    if head_position[1] - tail_position[1] < -1 {
+        //DOWN
+        if head_position[0] == tail_position[0] {
+            tail_position[1] -= 1;
+        } else if head_position[0] > tail_position[0] {
+            tail_position[1] -= 1;
+            tail_position[0] += 1;
+        } else {
+            tail_position[1] -= 1;
+            tail_position[0] -= 1;
+        }
+    }
+    if head_position[0] - tail_position[0] > 1 {
+        //RIGHT
+        if head_position[1] == tail_position[1] {
+            tail_position[0] += 1;
+        } else if head_position[1] > tail_position[1] {
+            tail_position[0] += 1;
+            tail_position[1] += 1;
+        } else {
+            tail_position[0] += 1;
+            tail_position[1] -= 1;
+        }
+    }
+    if head_position[0] - tail_position[0] < -1 {
+        //LEFT
+        if head_position[1] == tail_position[1] {
+            tail_position[0] -= 1;
+        } else if head_position[1] > tail_position[1] {
+            tail_position[0] -= 1;
+            tail_position[1] += 1;
+        } else {
+            tail_position[0] -= 1;
+            tail_position[1] -= 1;
+        }
+    }
+    tail_positions.insert((tail_position[0], tail_position[1]));
+}
+
+fn move_tail(
+    head_position: &mut Vec<i32>,
+    tail_position: &mut Vec<i32>,
+    tail_positions: &mut HashSet<(i32, i32)>,
+) {
+    if (head_position[0] - tail_position[0]).abs() > 1
+        || (head_position[1] - tail_position[1]).abs() > 1
+    {
+        tail_position[0] += (head_position[0] - tail_position[0]).signum();
+        tail_position[1] += (head_position[1] - tail_position[1]).signum();
+        tail_positions.insert((tail_position[0], tail_position[1]));
+    }
+}
+
+fn move_up(
+    head_position: &mut Vec<i32>,
+    tail_position: &mut Vec<i32>,
+    steps: u32,
+    tail_positions: &mut HashSet<(i32, i32)>,
+) {
+    for _ in 0..steps {
+        head_position[1] += 1;
+        move_tail(head_position, tail_position, tail_positions);
+    }
+}
+
+fn move_down(
+    head_position: &mut Vec<i32>,
+    tail_position: &mut Vec<i32>,
+    steps: u32,
+    tail_positions: &mut HashSet<(i32, i32)>,
+) {
+    for _ in 0..steps {
+        head_position[1] -= 1;
+        move_tail(head_position, tail_position, tail_positions);
+    }
+}
+
+fn move_right(
+    head_position: &mut Vec<i32>,
+    tail_position: &mut Vec<i32>,
+    steps: u32,
+    tail_positions: &mut HashSet<(i32, i32)>,
+) {
+    for _ in 0..steps {
+        head_position[0] += 1;
+        move_tail(head_position, tail_position, tail_positions);
+    }
+}
+
+fn move_left(
+    head_position: &mut Vec<i32>,
+    tail_position: &mut Vec<i32>,
+    steps: u32,
+    tail_positions: &mut HashSet<(i32, i32)>,
+) {
+    for _ in 0..steps {
+        head_position[0] -= 1;
+        move_tail(head_position, tail_position, tail_positions);
+    }
+}
+
+fn first_question(lines: io::Lines<io::BufReader<File>>) -> io::Result<usize> {
+    let input_lines: Vec<String> = lines.map(|line| line.unwrap()).collect();
+    let mut head_position = vec![0, 0];
+    let mut tail_position = vec![0, 0];
+    let mut tail_positions = HashSet::from([(0, 0)]);
+    //tail_positions.insert("0_0".to_string());
+    for line in input_lines {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let direction = parts[0];
+        let steps = parts[1].parse::<u32>().unwrap();
+        match direction {
+            "U" => move_up(
+                &mut head_position,
+                &mut tail_position,
+                steps,
+                &mut tail_positions,
+            ),
+            "D" => move_down(
+                &mut head_position,
+                &mut tail_position,
+                steps,
+                &mut tail_positions,
+            ),
+            "R" => move_right(
+                &mut head_position,
+                &mut tail_position,
+                steps,
+                &mut tail_positions,
+            ),
+            "L" => move_left(
+                &mut head_position,
+                &mut tail_position,
+                steps,
+                &mut tail_positions,
+            ),
+            _ => unreachable!("WRONG MOVE"),
+        };
+    }
+    Ok(tail_positions.len())
+}
+
+fn second_question(lines: io::Lines<io::BufReader<File>>) -> io::Result<usize> {
+    let input_lines: Vec<String> = lines.map(|line| line.unwrap()).collect();
+    let mut rope_positions: Vec<Vec<i32>> = vec![];
+    let mut tail_positions = vec![];
+    for _ in 0..10 {
+        //init structures
+        rope_positions.push(vec![0, 0]);
+        tail_positions.push(HashSet::from([(0, 0)]));
+    }
+    for line in input_lines {
+        let parts: Vec<&str> = line.split(" ").collect();
+        let direction = parts[0];
+        let steps = parts[1].parse::<u32>().unwrap();
+        for _ in 0..steps {
+            let splits = rope_positions.split_at_mut(1);
+            let mut head_position = &mut splits.0[0];
+            let mut tail_position = &mut splits.1[0];
+            match direction {
+                "U" => move_up(
+                    &mut head_position,
+                    &mut tail_position,
+                    1,
+                    &mut tail_positions[1],
+                ),
+                "D" => move_down(
+                    &mut head_position,
+                    &mut tail_position,
+                    1,
+                    &mut tail_positions[1],
+                ),
+                "R" => move_right(
+                    &mut head_position,
+                    &mut tail_position,
+                    1,
+                    &mut tail_positions[1],
+                ),
+                "L" => move_left(
+                    &mut head_position,
+                    &mut tail_position,
+                    1,
+                    &mut tail_positions[1],
+                ),
+                _ => unreachable!("WRONG MOVE"),
+            };
+            for k in 1..rope_positions.len() - 1 {
+                let splits = rope_positions.split_at_mut(k + 1);
+                let mut head_position = &mut splits.0[k];
+                let mut tail_position = &mut splits.1[0];
+                move_tail(
+                    &mut head_position,
+                    &mut tail_position,
+                    &mut tail_positions[k + 1],
+                );
+            }
+        }
+    }
+
+    Ok(tail_positions[tail_positions.len() - 1].len())
+}
